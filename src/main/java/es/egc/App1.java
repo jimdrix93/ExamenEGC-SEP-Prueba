2a69be1d9ce3c56c5bc4fc673425c661291a84c6
jimdrix93 <javiihv93@gmail.com>
2018-09-09 17:03:00 +0200

    Arreglo nombre del metodo

--- src/main/java/es/egc/App1.java
+++ src/main/java/es/egc/App1.java
@@ -15,7 +15,7 @@ public class App1
 		System.out.println(a + "*" + b + " = " + App1.multiplica(a,b));
 	}
 	
-	public static int divide(int a, int b){
+	public static int multiplica(int a, int b){
 		int result= a*b;
 		log.info("Returning "+a+"*"+b+"="+result);
 		return result;
